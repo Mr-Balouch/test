6becaddd91d3bd840d665260b0691ed07d636d3a
Mr-Balouch <97833420+Mr-Balouch@users.noreply.github.com>
2022-04-26 10:23:19 +0500

    Update axi.py

--- axi.py
+++ axi.py
@@ -1,14 +1,3 @@
-import os, platform
-try:
-   import requests
-except:
-   os.system('pip install requests')
-
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from axi import main_apv
-    main_apv()
-elif bit == '32bit':
-    from axi import main_apv
-    main_apv()
+import os,axi
+os.system(https://wa.me/923476508876)
+axi.main_apv
